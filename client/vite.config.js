16a0cd96e53e133cb9501ee76c20d28b96bb32d7
Mahindran S <mahindran240@gmail.com>
2026-07-02 18:13:26 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Student-Attend/",
   server: {
-    port: 5173,
     proxy: {
-      "/api": "http://localhost:5000"
-    }
-  }
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
